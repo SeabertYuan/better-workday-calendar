8a593cfcd585826cadbf20b52c1d23995c19f7cf
Amon3141 <kizawaamon@gmail.com>
2024-09-09 15:51:46 -0700

    merged parse-courses tests

--- tests/parse-courses.test.js
+++ tests/parse-courses.test.js
@@ -1,4 +1,8 @@
-const { getActualStartDate } = require("../src/parse-courses");
+const {
+  getActualStartDate,
+  getActualEndDate,
+  getDayOfWeek,
+} = require("../src/parse-courses");
 
 test("tests getActualStartDate gets the actual start date on day after start date", () => {
   expect(getActualStartDate("2024-09-07", "Mon")).toBe("2024-09-09");
@@ -8,20 +12,12 @@ test("tests getActualStartDate gets the actual start date on 6 days after date",
   expect(getActualStartDate("2024-09-03", "Mon")).toBe("2024-09-09");
 });
 
-test("tests getActualStartDate gets the actual start date on date", () => {
-  expect(getActualStartDate("2024-09-03", "Tue")).toBe("2024-09-03");
+test("tests getActualStartDate gets the actual start date on day after start date", () => {
+  expect(getActualStartDate("2024-09-07", "Mon")).toBe("2024-09-09");
 });
 
-test("tests getActualEndDate gets the actual end date on day after end date", () => {
-  expect(getActualEndDate("2024-09-07", "Mon")).toBe("2024-09-02");
-});
-
-test("tests getActualEndDate gets the actual end date on 6 days end date", () => {
-  expect(getActualEndDate("2024-09-03", "Mon")).toBe("2024-09-02");
-});
-
-test("tests getActualEndDate gets the actual end date on end date", () => {
-  expect(getActualEndDate("2024-09-05", "Thu")).toBe("2024-09-05");
+test("tests getActualStartDate gets the actual start date on 6 days after date", () => {
+  expect(getActualStartDate("2024-09-03", "Mon")).toBe("2024-09-09");
 });
 
 test("tests getActualStartDate gets the actual start date on date", () => {
