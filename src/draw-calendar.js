0eb1d44ba0084b110f1f986d277a4dd549a7dfe4
Amon3141 <kizawaamon@gmail.com>
2024-09-13 22:33:37 -0700

    fixed class-name bug, updated version

--- src/draw-calendar.js
+++ src/draw-calendar.js
@@ -93,7 +93,7 @@ async function updateDayOfWeek() {
 }
 
 function addDayOfWeekEventListener() {
-  const buttons = document.querySelectorAll("ul.WB31 li");
+  const buttons = document.querySelectorAll("ul.WL41 li");
   if (!buttons) return;
   buttons.forEach(button => {
     button.addEventListener("click", updateDayOfWeek);
